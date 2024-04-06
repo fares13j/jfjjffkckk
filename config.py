030f4cbd9d974f191589a58c1eaac07abf196b19
fares13j <163900595+fares13j@users.noreply.github.com>
2024-04-06 18:39:34 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+# You Can Get API ID And API HASH From: https://core.telegram.org/api/obtaining_api_id
+
+API_ID = int(os.getenv("25535561", "YourAPI_ID"))
+
+API_HASH = os.getenv("f40dbe709ac3d5b8a18aed1edb9b0d04", "YourAPI_Hash")
+
+BOT_TOKEN = os.getenv("7139148015:AAE6R6xMjiRslP9tT0afttth6G8vRd-qFdo", "YourBotToken") #Telegram BoT Token, You Can Create One From: @slialisidyahya_bot
+
+ADMIN = int(os.getenv("6789046153", "TheBotAdminAccountID")) #The ID of Account That Will Use The BoT
+
